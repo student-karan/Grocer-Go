9061ee1d115dda2ae2f91495ab66f58b7ea292ce
Jasmeet Singh <karan9310446410@gmail.com>
2025-08-24 18:37:28 +0530

    second commit

--- Client/src/pages/Home/BestSellers.tsx
+++ Client/src/pages/Home/BestSellers.tsx
@@ -2,7 +2,6 @@ import ProductCard from '../../components/ProductCard.tsx';
 import { type product } from '../../Helpers/types.ts';
 
 const BestSellers = ({ products }: { products: product[] }) => {
-    let BestProducts = products.filter((product) => product.inStock);
     return (
         <div className="section_body">
             <div className="section_heading_container">
@@ -11,7 +10,7 @@ const BestSellers = ({ products }: { products: product[] }) => {
             </div>
             <div className='all_products'>
                 {products.length == 0 && <h2>No Products Found</h2>}
-                {BestProducts.map((product, idx) => {
+                {products.map((product, idx) => {
                     return (<ProductCard key={idx} product={product} />)
                 })}
             </div>
