97da71a302b46e64727105751c4caa349f560de3
Jasmeet Singh <karan9310446410@gmail.com>
2025-08-24 19:06:13 +0530

    some changes

--- Client/src/Helpers/utils.ts
+++ Client/src/Helpers/utils.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "https://grocer-go-pi.vercel.app/api",
     withCredentials: true
 })
