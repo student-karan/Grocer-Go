b3c97aa2018d7585a5f5f90e84db9034827f21cd
Jasmeet Singh <karan9310446410@gmail.com>
2025-08-24 15:18:54 +0530

    second commit

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -47,10 +47,10 @@ app.use("/api/address", AddressRouter);
 app.use("/api/order",OrderRouter);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../Client/dist")));
+    app.use(express.static(path.join(__dirname,"../../Client/dist")));
 
     app.get(/.*/,(req:Request,res:Response)=>{
-        res.sendFile(path.join(__dirname,"../Client/dist/index.html"));
+        res.sendFile(path.join(__dirname,"../../Client/dist/index.html"));
     })
 }
 
